abd01b9bf75cb5cc8875b94f48277e4351ce805e
kr-raghav123 <108853134+kr-raghav123@users.noreply.github.com>
2022-07-10 18:08:27 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is a React Project</h1>
+      <p>
+        Making a project note taking app with the help of javascript css and react.js <react className="js"></react> <react className="js"></react>
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
